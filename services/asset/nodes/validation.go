4008244f9cf721cdd4546d61a567c7ba24d74abd
N1ark <n1ark.officiel@gmail.com>
2022-08-25 15:31:44 +0200

    SDEVOPS-195 - Move already present validation to `validation.go`

--- services/asset/nodes/validation.go
+++ services/asset/nodes/validation.go
@@ -3,26 +3,33 @@ package nodes
 import (
 	"fmt"
 	"regexp"
+
+	. "leanspace-terraform-provider/helper"
 )
 
 var tle1stLineRegex = regexp.MustCompile(`^1 (?P<noradId>[ 0-9]{5})[A-Z] [ 0-9]{5}[ A-Z]{3} [ 0-9]{5}[.][ 0-9]{8} (?:(?:[ 0+-][.][ 0-9]{8})|(?: [ +-][.][ 0-9]{7})) [ +-][ 0-9]{5}[+-][ 0-9] [ +-][ 0-9]{5}[+-][ 0-9] [ 0-9] [ 0-9]{4}[ 0-9]$`)
 var tle2ndLineRegex = regexp.MustCompile(`^2 (?P<noradId>[ 0-9]{5}) [ 0-9]{3}[.][ 0-9]{4} [ 0-9]{3}[.][ 0-9]{4} [ 0-9]{7} [ 0-9]{3}[.][ 0-9]{4} [ 0-9]{3}[.][ 0-9]{4} [ 0-9]{2}[.][ 0-9]{13}[ 0-9]$`)
 
-func (node *Node) Validate() error {
-	if node.Type == "ASSET" && !(node.Kind == "GENERIC" || node.Kind == "SATELLITE" || node.Kind == "GROUND_STATION") {
-		return fmt.Errorf("kind must be either GENERIC, SATELLITE ou GROUND_STATION, got: %q", node.Kind)
-	}
+var nodeValidators = Validators{
+	Equivalence(
+		Equals("type", "ASSET"),
+		IsSet("kind"),
+	),
+	If(
+		Not(Equals("kind", "SATELLITE")),
+		Or(Not(IsSet("tle")), IsEmpty("tle")),
+	),
+	If(
+		And(IsSet("tle"), Not(IsEmpty("tle"))),
+		HasLength("tle", 2),
+	),
+}
 
-	if node.Kind != "SATELLITE" && node.Tle != nil && len(node.Tle) != 0 {
-		return fmt.Errorf("TLE must only be specified for satellites, got %q", node.Tle)
+func (node *Node) Validate(obj map[string]any) error {
+	if err := nodeValidators.Check(obj); err != nil {
+		return err
 	}
-
 	if node.Kind == "SATELLITE" && node.Tle != nil {
-
-		if len(node.Tle) != 2 {
-			return fmt.Errorf("if a TLE is specified, it must contain only two strings, got %q", node.Tle)
-		}
-
 		if !tle1stLineRegex.MatchString(node.Tle[0]) {
 			return fmt.Errorf("TLE first line mutch match %q, got: %q", tle1stLineRegex, node.Tle[0])
 		}
---
+++ services/monitors/monitors/validation.go
@@ -0,0 +1,16 @@
+package monitors
+
+import (
+	. "leanspace-terraform-provider/helper"
+)
+
+var monitorExpressionValidators = Validators{
+	If(
+		IsSet("tolerance"),
+		Or(Equals("comparison_operator", "EQUAL_TO"), Equals("comparison_operator", "NOT_EQUAL_TO")),
+	),
+}
+
+func (monitor *Monitor) Validate(obj map[string]any) error {
+	return monitorExpressionValidators.Check(obj["expression"].([]any)[0].(map[string]any))
+}
