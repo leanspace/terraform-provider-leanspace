e6968bd2b486f017199dfe3d3d127ec714dd9342
Elodie TINLAND <elodie@leanspace.io>
2023-01-30 15:48:40 +0100

    BLUE-1104 : Update data source for properties example usage

--- examples/data_sources/leanspace_properties.tf
+++ examples/data_sources/leanspace_properties.tf
@@ -1,10 +1,9 @@
 data "leanspace_properties" "all" {
   filters {
     node_ids   = [var.node_id]
-    node_types = ["ASSET"]
-    node_kinds = ["SATELLITE"]
+    built_in   = []
+    names      = []
     tags       = []
-    ids        = []
     query      = ""
     page       = 0
     size       = 10
