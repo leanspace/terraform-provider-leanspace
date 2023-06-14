b67d34899cf2f5a08abb67a0cadbbd0d5fa36957
Florian <florian@leanspace.io>
2023-06-14 16:01:58 +0200

    GOLDS-3 - Monitors update terraform configuration according to new API

--- services/monitors/monitors/models.go
+++ services/monitors/monitors/models.go
@@ -6,39 +6,26 @@ import (
 )
 
 type Monitor struct {
-	ID                        string                            `json:"id"`
-	Name                      string                            `json:"name"`
-	Description               string                            `json:"description"`
-	Status                    string                            `json:"status"`
-	PollingFrequencyInMinutes int                               `json:"pollingFrequencyInMinutes"`
-	MetricId                  string                            `json:"metricId"`
-	NodeId                    string                            `json:"nodeId"`
-	Statistics                Statistics                        `json:"statistics"`
-	Expression                Expression                        `json:"expression"`
-	ActionTemplates           []action_templates.ActionTemplate `json:"actionTemplates"`
-	ActionTemplateIds         []string                          `json:"actionTemplateIds"`
-	Tags                      []general_objects.KeyValue        `json:"tags"`
-	CreatedAt                 string                            `json:"createdAt"`
-	CreatedBy                 string                            `json:"createdBy"`
-	LastModifiedAt            string                            `json:"lastModifiedAt"`
-	LastModifiedBy            string                            `json:"lastModifiedBy"`
+	ID                string                            `json:"id"`
+	Name              string                            `json:"name"`
+	Description       string                            `json:"description"`
+	Status            string                            `json:"status"`
+	MetricId          string                            `json:"metricId"`
+	NodeId            string                            `json:"nodeId"`
+	Rule              Rule                              `json:"rule"`
+	ActionTemplates   []action_templates.ActionTemplate `json:"actionTemplates"`
+	ActionTemplateIds []string                          `json:"actionTemplateIds"`
+	Tags              []general_objects.KeyValue        `json:"tags"`
+	CreatedAt         string                            `json:"createdAt"`
+	CreatedBy         string                            `json:"createdBy"`
+	LastModifiedAt    string                            `json:"lastModifiedAt"`
+	LastModifiedBy    string                            `json:"lastModifiedBy"`
 }
 
 func (monitor *Monitor) GetID() string { return monitor.ID }
 
-type Statistics struct {
-	LastEvaluation Evaluation `json:"lastEvaluation"`
-}
-
-type Evaluation struct {
-	Timestamp string  `json:"timestamp"`
-	Value     float64 `json:"value"`
-	Status    string  `json:"status"`
-}
-
-type Expression struct {
-	ComparisonOperator  string  `json:"comparisonOperator"`
-	ComparisonValue     float64 `json:"comparisonValue"`
-	AggregationFunction string  `json:"aggregationFunction"`
-	Tolerance           float64 `json:"tolerance,omitempty"`
+type Rule struct {
+	ComparisonOperator string  `json:"comparisonOperator"`
+	ComparisonValue    float64 `json:"comparisonValue"`
+	Tolerance          float64 `json:"tolerance,omitempty"`
 }
