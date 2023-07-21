fa3bd5890ec100a89b4dbd10f4078a8a712da63a
Florian <florian@leanspace.io>
2023-07-21 09:56:31 +0200

    GOLDS-282 - team&role team expose credential on leanspce_service_account

--- services/teams/service_accounts/models.go
+++ services/teams/service_accounts/models.go
@@ -1,13 +1,19 @@
 package service_accounts
 
 type ServiceAccount struct {
-	ID             string   `json:"id"`
-	Name           string   `json:"name"`
-	PolicyIds      []string `json:"policyIds"`
-	CreatedAt      string   `json:"createdAt"`
-	CreatedBy      string   `json:"createdBy"`
-	LastModifiedAt string   `json:"lastModifiedAt"`
-	LastModifiedBy string   `json:"lastModifiedBy"`
+	ID             string      `json:"id"`
+	Name           string      `json:"name"`
+	PolicyIds      []string    `json:"policyIds"`
+	Credendials    Credentials `json:"credentials"`
+	CreatedAt      string      `json:"createdAt"`
+	CreatedBy      string      `json:"createdBy"`
+	LastModifiedAt string      `json:"lastModifiedAt"`
+	LastModifiedBy string      `json:"lastModifiedBy"`
 }
 
 func (serviceAccount *ServiceAccount) GetID() string { return serviceAccount.ID }
+
+type Credentials struct {
+	ClientId     string `json:"clientId"`
+	ClientSecret string `json:"clientSecret"`
+}
--- services/teams/service_accounts/parsers.go
+++ services/teams/service_accounts/parsers.go
@@ -1,18 +1,33 @@
 package service_accounts
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
 
 func (serviceAccount *ServiceAccount) ToMap() map[string]any {
 	serviceAccountMap := make(map[string]any)
 	serviceAccountMap["id"] = serviceAccount.ID
 	serviceAccountMap["name"] = serviceAccount.Name
 	serviceAccountMap["policy_ids"] = serviceAccount.PolicyIds
+	serviceAccountMap["credentials"] = []any{serviceAccount.Credendials.ToMap()}
 	serviceAccountMap["created_at"] = serviceAccount.CreatedAt
 	serviceAccountMap["created_by"] = serviceAccount.CreatedBy
 	serviceAccountMap["last_modified_at"] = serviceAccount.LastModifiedAt
 	serviceAccountMap["last_modified_by"] = serviceAccount.LastModifiedBy
-
 	return serviceAccountMap
+}
+
+func (credential *Credentials) FromMap(credentialMap map[string]any) error {
+	credential.ClientId = credentialMap["client_id"].(string)
+	credential.ClientSecret = credentialMap["client_secret"].(string)
+	return nil
+}
+
+func (credential *Credentials) ToMap() map[string]any {
+	credentialMap := make(map[string]any)
+	credentialMap["client_id"] = credential.ClientId
+	credentialMap["client_secret"] = credential.ClientSecret
+	return credentialMap
 }
 
 func (serviceAccount *ServiceAccount) FromMap(serviceAccountMap map[string]any) error {
@@ -21,6 +36,12 @@ func (serviceAccount *ServiceAccount) FromMap(serviceAccountMap map[string]any) 
 	serviceAccount.PolicyIds = make([]string, serviceAccountMap["policy_ids"].(*schema.Set).Len())
 	for i, value := range serviceAccountMap["policy_ids"].(*schema.Set).List() {
 		serviceAccount.PolicyIds[i] = value.(string)
+	}
+
+	if len(serviceAccountMap["credentials"].([]any)) > 0 {
+		if err := serviceAccount.Credendials.FromMap(serviceAccountMap["credentials"].([]any)[0].(map[string]any)); err != nil {
+			return err
+		}
 	}
 	serviceAccount.CreatedAt = serviceAccountMap["created_at"].(string)
 	serviceAccount.CreatedBy = serviceAccountMap["created_by"].(string)
