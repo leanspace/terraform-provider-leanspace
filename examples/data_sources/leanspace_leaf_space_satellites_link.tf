4de5a29542970edd5152b165a03807596a7f978b
Florian <florian@leanspace.io>
2023-08-30 14:59:57 +0200

    GOLDS-264 - LeafSpace terraform provider correct doc

---
+++ examples/data_sources/leanspace_leaf_space_satellites_link.tf
@@ -0,0 +1,12 @@
+
+data "leanspace_leaf_space_satellites_link" "all" {
+  filters {
+    leafspace_satellite_ids = ["c736e7ed36916f5d4c14e454087a3dc2"]
+    leanspace_satellite_ids = ["3ff7b5e4-0a5c-4d32-99a6-1f366ee3d069"]
+    ids                = []
+    query              = ""
+    page               = 0
+    size               = 10
+    sort               = ["asc"]
+  }
+}
---
+++ examples/resources/leanspace_leaf_space_ground_station_links.tf
@@ -0,0 +1,5 @@
+resource "leanspace_leaf_space_ground_station_links" "ground_station_link" {
+  leafspace_ground_station_id = "d5de2269dc23179929546f41b6239afb"
+  leanspace_ground_station_id = "969e157d-8883-43cd-b851-3d7ff3449ec6"
+
+}
