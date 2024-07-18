6a5b6fdc2d69bca1d85809566d6f1af1ada39f21
Marc-Sev <marc@leanspace.io>
2024-07-18 10:50:16 +0200

    BLUE-1879: remove temp plan state naming

--- testing/plans/plan_states/main.tf
+++ testing/plans/plan_states/main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 }
 
-data "leanspace_new_plan_states" "all" {
+data "leanspace_plan_states" "all" {
 
 }
 
-resource "leanspace_new_plan_states" "created" {
+resource "leanspace_plan_states" "created" {
   name = "MY_TEST"
 }
 
 output "created" {
-  value = leanspace_new_plan_states.created
+  value = leanspace_plan_states.created
 }
