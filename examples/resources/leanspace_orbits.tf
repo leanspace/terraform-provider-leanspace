0025286ba544a8247187b712de108b714488110c
Elodie TINLAND <117275814+elodietinland@users.noreply.github.com>
2024-09-25 11:38:45 +0200

    BLUE-2005 : Update orbit example (#184)

--- examples/resources/leanspace_orbits.tf
+++ examples/resources/leanspace_orbits.tf
@@ -12,7 +12,27 @@ resource "leanspace_orbits" "an_orbit" {
     right_ascension_of_ascending_node = 50.0
     argument_of_perigee               = 0.8
     altitude_in_meters                = 150.0
-    eccentricity                      = 0.7
+    eccentricity                      = 0.999
+  }
+  gps_configuration {
+    gps_metrics {
+      metric_id_for_position_x = var.metric_id_for_position_x
+      metric_id_for_position_y = var.metric_id_for_position_y
+      metric_id_for_position_z = var.metric_id_for_position_z
+      metric_id_for_velocity_x = var.metric_id_for_velocity_x
+      metric_id_for_velocity_y = var.metric_id_for_velocity_y
+      metric_id_for_velocity_z = var.metric_id_for_velocity_z
+    }
+    standard_deviations {
+      latitude  = 0.2
+      longitude = 0.2
+      altitude  = 100
+      ground_speed = 10
+    }
+  }
+  satellite_configuration {
+    drag_cross_section = 35.3
+    radiation_cross_section = 55.2
   }
   tags {
     key   = "Mission"
