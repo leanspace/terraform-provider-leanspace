deb136c6d1b183f5815cb2bc0b28501ec13008a4
Marc-Sev <marc@leanspace.io>
2024-12-30 11:44:08 +0100

    BLUE-2092: remove local testing creds

--- testing/requests/request_definitions/main.tf
+++ testing/requests/request_definitions/main.tf
@@ -2,16 +2,8 @@ terraform {
   required_providers {
     leanspace = {
       source = "leanspace/leanspace"
-      version = "8.1.5"
     }
   }
-}
-
-provider "leanspace" {
-  env           = "develop"
-  tenant        = "yuri"
-  client_id     = "nlbja2p65j8kj7of0tfs29rf4"
-  client_secret = "d762kk9862jn0j1qr4c2u3o8bjkv70o45pld3200ek89qtul6kg"
 }
 
 variable "plan_template_id" {
