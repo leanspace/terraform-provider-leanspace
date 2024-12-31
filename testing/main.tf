c4ac66af58cdcb5551780c27282567e04b0f4387
jerome-leanspace <jerome@leanspace.io>
2024-12-31 11:53:37 +0100

    BUZZ-000: Add workflow

--- testing/main.tf
+++ testing/main.tf
@@ -7,10 +7,10 @@ terraform {
 }
 
 provider "leanspace" {
-  tenant        = "yuri"
-  env           = "develop"
-  client_id     = "nlbja2p65j8kj7of0tfs29rf4"
-  client_secret = "d762kk9862jn0j1qr4c2u3o8bjkv70o45pld3200ek89qtul6kg"
+  tenant        = var.tenant
+  env           = var.env
+  client_id     = var.client_id
+  client_secret = var.client_secret
 }
 
 module "nodes" {
