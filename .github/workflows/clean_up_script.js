abefdc9147cbe12cd3fefc308cf3a49a43591f82
jerome-leanspace <jerome@leanspace.io>
2025-02-27 11:15:16 +0100

    BUZZ-000: testing auto fetching of services

--- .github/workflows/clean_up_script.js
+++ .github/workflows/clean_up_script.js
@@ -1,4 +1,4 @@
-module.exports = async (tenant, env, client_id, client_secret) => {
+module.exports = async (service_list, tenant, client_id, client_secret) => {
 	async function delete_resource(endpoint, headers) {
 		console.log(endpoint);
 		const delete_service = await fetch(endpoint, {
@@ -23,26 +23,7 @@ module.exports = async (tenant, env, client_id, client_secret) => {
 		"Content-type": "application/json; charset=UTF-8",
 		"Authorization": `Bearer ${token}`
 	};
-	const services = ['activities-repository/activity-definitions?query=Terraform%','activities-repository/activities/states?query=MY_TEST','activities-repository/activity-definitions/resource-functions?query=Terraform%',
-		'agents-repository/agents?query=Terraform%',
-		`asset-repository/nodes?createdBy=${client_id}`, `asset-repository/properties/v2?createdBy=${client_id}`, 'asset-repository/units?query=%Customium',
-		"commands-repository/command-definitions?createdBys=${createdBy}", 'commands-repository/command-queues?query=Terraform%', 'commands-repository/command-sequences/states?query=MY_TEST', 'commands-repository/command-sequences/commands/states?query=MY_TEST',`commands-repository/release-queues?createdBys=${client_id}`,
-		'dashboard-repository/dashboards?query=Terraform%', 'dashboard-repository/widgets?query=Terraform%',
-		'events/event-definitions?query=Terraform%',
-		'integration-leafspace/connections', 'integration-leafspace/contact-reservations/status/mappings?query=terraform test', 'integration-leafspace/ground-stations/links??leafspaceGroundStationIds=d5de2269dc23179929546f41b6239afb', 'integration-leafspace/satellites/links?leafspaceSatelliteIds=c736e7ed36916f5d4c14e454087a3dc2',
-		'metrics-repository/metrics?query=Terra%',
-		'monitors-repository/action-templates?query=Terraform&','monitors-repository/monitors?query=Terraform%',
-		`orbits-repository/orbits?createdBys=${client_id}`,
-		'passes-repository/contacts/states?query=MY_TEST', 'passes-repository/passes/delay/configurations', 'passes-repository/passes/states?query=MY_TEST',
-		'plans-repository/plans/states?query=MY_TEST', `plans-repository/plan-templates?createdBys=${client_id}`,
-		'plugins-repository/generic-plugins?query=Terraform%', 'plugins-repository/plugins?query=Terraform%',
-		'records/record-templates?query=Terraform%',
-		`requests-repository/feasibility-constraint-definitions?createdBys=${client_id}`, 'requests-repository/request-definitions?query=%Terraform', 'requests-repository/requests/states?query=TERRAFORM_STATE',
-		`resources-repository/resources?createdBys=${client_id}`,
-		`routes-repository/processors?createdBys=${client_id}`, 'routes-repository/routes?query=Terraform%',
-		'streams-repository/streams?query=Terraform%',
-		'teams-repository/access-policies?query=Terraform%', 'teams-repository/members?query=Terra%', 'teams-repository/service-accounts?query=Terra%', 'teams-repository/teams?query=Terra%',
-	];
+	const services = service_list.split(',');
 
 	for (const service of services) {
 		const endpoint = encodeURI(`https://api.develop.leanspace.io/${service}`);
