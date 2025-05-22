0608751756bae54d4639bd08d19f70e3b2965da9
Jérôme Boeglin <92445098+jerome-leanspace@users.noreply.github.com>
2025-05-22 16:10:37 +0200

    GOLDS-1319: change leaf space's sandbox api (#238)

--- testing/leaf_space_integration/connections/main.tf
+++ testing/leaf_space_integration/connections/main.tf
@@ -6,10 +6,15 @@ terraform {
   }
 }
 
+variable password {
+  description = "Password for the Leafspace account"
+  type        = string
+}
+
 
 resource "leanspace_leaf_space_integrations" "integration_Connection" {
   name       = "Leafspace Connection Terraform"
   username   = "leanspace01"
-  password   = "vVVy5syuUuSk9MVkRfvYm8Xp6Nhy6qAs"
-  domain_url = "sandbox.api.leaf.space"
+  password   = var.password
+  domain_url = "apiv2.sandbox.leaf.space"
 }
