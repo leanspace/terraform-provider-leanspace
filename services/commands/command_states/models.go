b1806c55fa244a1404529ca03f008426a29aa1e0
Florian <florian@leanspace.io>
2025-09-23 11:35:10 +0200

    GOLDS-1553 - add tags API on commands state

--- services/commands/command_states/models.go
+++ services/commands/command_states/models.go
@@ -1,13 +1,16 @@
 package command_states
 
+import "github.com/leanspace/terraform-provider-leanspace/helper/general_objects"
+
 type CommandState struct {
-	ID             string `json:"id"`
-	Name           string `json:"name"`
-	ReadOnly       bool   `json:"readOnly"`
-	CreatedAt      string `json:"createdAt"`
-	CreatedBy      string `json:"createdBy"`
-	LastModifiedAt string `json:"lastModifiedAt"`
-	LastModifiedBy string `json:"lastModifiedBy"`
+	ID             string                     `json:"id"`
+	Name           string                     `json:"name"`
+	ReadOnly       bool                       `json:"readOnly"`
+	Tags           []general_objects.KeyValue `json:"tags,omitempty"`
+	CreatedAt      string                     `json:"createdAt"`
+	CreatedBy      string                     `json:"createdBy"`
+	LastModifiedAt string                     `json:"lastModifiedAt"`
+	LastModifiedBy string                     `json:"lastModifiedBy"`
 }
 
 func (state *CommandState) GetID() string { return state.ID }
