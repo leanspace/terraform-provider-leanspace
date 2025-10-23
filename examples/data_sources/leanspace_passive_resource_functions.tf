e40db23806b1c0faff78c8ddd0a0c9fcf07a1551
Elodie TINLAND <elodie@leanspace.io>
2025-10-23 15:48:04 +0200

    BLUE-2831: Fix index in data sources

--- examples/data_sources/leanspace_passive_resource_functions.tf
+++ examples/data_sources/leanspace_passive_resource_functions.tf
@@ -2,8 +2,8 @@ data "leanspace_passive_resource_functions" "all" {
   filters {
     ids                     = []
     resource_ids            = []
-    tags         = []
-    query        = ""
+    tags                    = []
+    query                   = ""
     page                    = 0
     size                    = 10
     sort                    = ["name,asc"]
