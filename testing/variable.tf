9c5adcc9e512e3ba21a58a47ff8c2f85b2bf17e6
Florian <florian@leanspace.io>
2025-10-30 13:57:07 +0100

    GOLDS-1588 - Add tags API on event criticality

--- testing/variable.tf
+++ testing/variable.tf
@@ -2,25 +2,20 @@
 
 variable "tenant" {
   type = string
-  default = "vostok"
 }
 
 variable "client_id" {
   type = string
-  default = "1o0vba7k2h85sg3qhakmdjl5ai"
 }
 
 variable "client_secret" {
   type = string
-  default = "11atj4j861vrq18v0bmjmd7eqrsgjaughaqvb8o18b87ih9u0510"
 }
 
 variable "env" {
   type = string
-  default = "develop"
 }
 
 variable "leaf_space_password" {
   type = string
 }
-
