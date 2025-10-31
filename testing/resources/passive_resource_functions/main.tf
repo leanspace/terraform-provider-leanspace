56b18708b48c0628ac83964a1ae95928b38f9054
coutinol <olivier@leanspace.io>
2025-10-31 11:16:57 +0100

    BLUE-2890 remove test code

--- testing/resources/passive_resource_functions/main.tf
+++ testing/resources/passive_resource_functions/main.tf
@@ -2,16 +2,8 @@ terraform {
   required_providers {
     leanspace = {
       source = "leanspace/leanspace"
-      version = "10.9.1"
     }
   }
-}
-
-provider "leanspace" {
-  tenant        = "yuri"
-  env           = "develop"
-  client_id     = "nlbja2p65j8kj7of0tfs29rf4"
-  client_secret = "d762kk9862jn0j1qr4c2u3o8bjkv70o45pld3200ek89qtul6kg"
 }
 
 variable "resource1_id" {
