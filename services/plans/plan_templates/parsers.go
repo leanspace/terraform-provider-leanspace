5aecc602c2012dfc467aae7c11c1d1a23a4e36d1
jerome-leanspace <jerome@leanspace.io>
2026-01-21 15:40:06 +0100

    BUZ-000: revert plan template change

--- services/plans/plan_templates/parsers.go
+++ services/plans/plan_templates/parsers.go
@@ -92,7 +92,7 @@ func (formula *ResourceFunctionFormula) ToMap() map[string]any {
 	}
 
 	if formula.Type == "RECTANGULAR" {
-		formulaMap["amplitude"] = *formula.Amplitude
+		formulaMap["amplitude"] = formula.Amplitude
 	}
 
 	return formulaMap
@@ -231,10 +231,7 @@ func (formula *ResourceFunctionFormula) FromMap(formulaMap map[string]any) error
 	}
 
 	if formula.Type == "RECTANGULAR" {
-		if amplitude := formulaMap["amplitude"]; amplitude != nil {
-			amp := amplitude.(float64)
-			formula.Amplitude = &amp
-		}
+		formula.Amplitude = formulaMap["amplitude"].(float64)
 	}
 
 	return nil
